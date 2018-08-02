3a97e3255bf60f26f1110a49964a1503e03f300e
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-08-02 18:17:22 +0100

    Adding the Twitter code

---
+++ twitter/README.md
@@ -0,0 +1,3 @@
+# Twitter
+
+R code that produces the code from the 538 blog post. 
---
+++ twitter/recreate_538.r
@@ -0,0 +1,24 @@
+library(ggplot2)
+
+twitter_data <- read.csv('/Users/iain/git/russian-troll-tweets/IRAhandle_tweets.csv?raw=true')
+
+df2 <- data.frame(table(format(as.Date(twitter_data$publish_date, "%d/%m/%Y"), "%Y-%m")))
+
+#png('/Users/iain/Desktop/538all.png', width=1080, height = 540, units = 'px')
+
+#ggplot(data=df2, aes(x=Var1, y=Freq)) + geom_bar(stat = "identity") + 
+#  xlab("Month") + 
+#  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+#dev.off()
+
+tags <- twitter_data[twitter_data$account_category== "HashtagGamer", ]
+df_tags <- data.frame(table(format(as.Date(tags$publish_date, "%d/%m/%Y"), "%Y-%m")))
+
+png('/Users/iain/Desktop/538gamer.png', width=1080, height = 540, units = 'px')
+
+ggplot() + 
+  geom_bar(stat = "identity", fill = "Black", data=df2, aes(x=Var1, y=Freq)) +
+  geom_bar(stat = "identity", fill = "Purple", data=df_tags, aes(x=Var1, y=Freq)) + 
+  xlab("Month") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+dev.off()
