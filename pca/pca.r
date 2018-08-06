8cbb3285b2c511fc78939829b0699c58b9122b6f
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-08-06 12:00:41 +0100

    Changed paths

--- pca/pca.r
+++ pca/pca.r
@@ -2,14 +2,13 @@
 library(ggplot2)
 library(jsonlite)
 
-f = read.csv('/Users/iain/Documents/phd/standford_pamphlets/data/Docuscope/Group1WholeNovelsDocuscopeClusterFigure6.1.txt')
+f = read.csv('/data/Docuscope/Group1WholeNovelsDocuscopeClusterFigure6.1.txt')
 
 df<-f[3:20]
 PC<-prcomp(df)
 PCi<-data.frame(PC$x, Words=f$Group)
-jsonpc <- write_json(PCi, '/Users/iain/Desktop/pca.json')
 
-png('/Users/iain/Desktop/pca.png', width=1080, height = 1080, units = 'px')
+png('pca.png', width=1080, height = 1080, units = 'px')
 ggplot(PCi,aes(x=PC1,y=PC2, col = Words))+
   geom_point(size=3,alpha=0.5)+
   theme_classic()
