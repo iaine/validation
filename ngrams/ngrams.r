dd66effa9e75697c545177d59ecba1c1ee609e75
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-08-06 12:01:18 +0100

    Adding in culturomics work

--- ngrams/ngrams.r
+++ ngrams/ngrams.r
@@ -6,16 +6,24 @@ convert_items <- function (dfA) {
 }
 #files <- list.files(path="/Users/iain/Desktop/ngram_data/")
 yearsdf <- read.table(file = "/Users/iain/Desktop/culturomics/googlebooks-eng-all-1gram-20120701-1", sep="\t", fill = T, quote = "")
-total <- read.table(file = "/Users/iain/Desktop/culturomics/googlebooks-eng-all-totalcounts-20120701.txt", header=F, sep="\t", fill = T, quote = "")
+total <- read.table(file = "/Users/iain/Desktop/culturomics/googlebooks-eng-all-totalcounts-20120701.txt", header=F, sep="\t", fill = T, quote = "", stringsAsFactors = F)
+
 
 total_counts = data.frame(year=integer(), match=integer(), volume=integer(), last=integer())
+
+n = apply(total, 1, function(y) { strsplit(y, ',') })
+h = as.data.frame.list(n)
+
 for (i in total) {
   tmp = convert_items(total$x[i])
-  print(tmp)
+
   counts=data.frame(year=tmp[0], match=tmp[1], volume=tmp[2], last=tmp[3])
   total_counts <- rbind(total_counts, counts)
 }
 
 years <- yearsdf[yearsdf$V1 %in% c(1850, 1910, 1950), ]
 
-ggplot(data=years, aes(x=V2, y=V3, col = V1)) + geom_line() + xlab("Years") + ylab("Frequency")
+ggplot(data=years, aes(x=V2, y=V3, col = V1)) + 
+  geom_line() + 
+  xlab("Years") + 
+  ylab("Frequency")
